9c42a899eb973cd43caa08816b71b06f2a99db18
William Blankenship <william.jblankenship@gmail.com>
2013-08-24 21:07:36 -0400

    Fixed button. Removed "Recieved:". Factored out functions for socket

--- client.js
+++ client.js
@@ -8,14 +8,21 @@ function firstRun() {
 		//All dependencies loaded
 		log.init("output");
 		socket = new WebSocket("ws://127.0.0.1:8080/socket");
-		socket.onopen = function() {
-			log.write("Connected!");
-		};
-		socket.onmessage = function(e) {
-			log.write("Received: "+e.data);
-		};
-		socket.onerror = function(e) {
-			console.log(e);
-		};
+		socket.onopen = function() {socketConnected();};
+		socket.onmessage = function(e) {socketMessage(e);};
+		socket.onerror = function(e) {socketError(e);};
 	});
+}
+
+function socketConnected() {
+	log.write("Connected!");
+}
+
+function socketMessage(e) {
+	//e is an event
+	log.write(e.data);
+}
+
+function socketError(e) {
+	log.write("Lost Connection");
 }
